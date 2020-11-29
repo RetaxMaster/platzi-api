1bb73a8df7157ed51ccfc180a36184eb960bcc4f
RetaxMaster <retaxmaster@hotmail.com>
2020-11-29 01:03:16 -0600

    Adicion de Laravel Horizon

---
+++ app/Console/Commands/TestJobCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\TestJob;
+use Illuminate\Console\Command;
+
+class TestJobCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:job';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test Job';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle() {
+
+        dispatch(new TestJob());
+
+    }
+}
